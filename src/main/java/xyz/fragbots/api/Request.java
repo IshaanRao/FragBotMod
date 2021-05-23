efb495ff9988d74fb2ee87cb8f20b1b8214eca5a
MySixSenses <66916362+MySixSenses@users.noreply.github.com>
2021-05-22 17:01:41 -0700

    Update Request.java
    Remove unnecessary user-agent descriptor.

--- src/main/java/xyz/fragbots/api/Request.java
+++ src/main/java/xyz/fragbots/api/Request.java
@@ -26,7 +26,6 @@ public class Request {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setConnectTimeout(5000);
             conn.setRequestMethod("GET");
-            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String input;
             StringBuilder response = new StringBuilder();
@@ -48,7 +47,6 @@ public class Request {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setConnectTimeout(5000);
             conn.setRequestMethod("GET");
-            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String input;
             StringBuilder response = new StringBuilder();
